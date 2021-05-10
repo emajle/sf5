888aaeae91d7b1696d0a3aa4388201dbbbba273e
Ema5-Zyon <emajoule@hotmail.fr>
2021-05-10 17:01:18 +0200

    add auto- style bootstrap

--- src/Form/LivreType.php
+++ src/Form/LivreType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Livre;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,15 @@ class LivreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('auteur')
-        ;
+            ->add('titre', TextType::class, [
+                "label" => "Titre du livre"
+            ])
+            ->add('auteur', TextType::class)
+            ->add("enregistrer", SubmitType::class, [
+                "attr" => [
+                    "class" => "btn btn-secondary"
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
