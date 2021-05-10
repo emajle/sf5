56df61be2c80d97bc43eb5023c45daae26446e65
Ema5-Zyon <emajoule@hotmail.fr>
2021-05-10 17:11:27 +0200

    add control form

--- src/Form/LivreType.php
+++ src/Form/LivreType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class LivreType extends AbstractType
 {
@@ -15,7 +17,15 @@ class LivreType extends AbstractType
     {
         $builder
             ->add('titre', TextType::class, [
-                "label" => "Titre du livre"
+                "label" => "Titre du livre",
+                "constraints" => [
+                    new Length([
+                        "min" => 4,
+                        "minMessage" => "Le titre doit avoir au moins 4 caractères",
+                        "max" => 50,
+                        "maxMessage" => "Le titre doit avoir maximum 50 caractères"
+                    ])
+                ]
             ])
             ->add('auteur', TextType::class)
             ->add("enregistrer", SubmitType::class, [
