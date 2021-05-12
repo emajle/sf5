7126c03532bffc0004fac72ceef1411b143de40b
Ema5-Zyon <emajoule@hotmail.fr>
2021-05-12 14:19:30 +0200

    create Catégorie CRUD

--- src/Controller/AbonneController.php
+++ src/Controller/AbonneController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface as Encoder;
 
-#[Route('/abonne')]
+#[Route('admin/abonne')]
 class AbonneController extends AbstractController
 {
     #[Route('/', name: 'abonne_index', methods: ['GET'])]
